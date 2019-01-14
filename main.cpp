bdefc7499795813b9b7234c39d40fa950a5668c6
vanek1821 <33785770+vanek1821@users.noreply.github.com>
2019-01-14 11:48:05 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -14,8 +14,24 @@ int main(int argc, const char * argv[]) {
     std::cout <<"Hello to server!\n";
     
     Server* server = new Server();
+    string adress = "127.0.0.1";
+    int port = 10000;
     
-    server->init();
+    if (argc<3){
+        cout << "Chybně zadané parametry" << endl;
+        cout << "1. parametr - adress" << endl;
+        cout << "2. parametr - port" << endl;
+        cout << "př.: ./server 127.0.0.1 10000";
+        return 0;
+    } else {
+        adress = argv[1];
+        port = stoi(argv[2]);
+    }
+    
+    if(server->init(adress, port) == -1){
+        cout << "Chyba inicializace serveru. Zkuste prosím znovu." << endl;
+        return 0;
+    }
     
     server->listenConnections();
     return 0;
